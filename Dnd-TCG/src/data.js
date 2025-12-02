4c5749a3bc11c62304349b6bb879b84536dd1b81
nathan reginato <nathan.reginato@gmail.com>
2025-12-02 09:21:28 -0700

    chore: Updates

--- Dnd-TCG/src/data.js
+++ Dnd-TCG/src/data.js
@@ -4,7 +4,7 @@ const player = {
   race: "Harringon",
   class: ["Bard"],
   specialization: ["Swashbuckler"],
-  background: "Guild Artisan",
+  background: "Celebrity Adventurer's Scion",
   alignment: "C/N",
   stats: [17, 12, 16, 19, 10, 9],
   proficientcies: ["Arc", "Inv"],
--- Dnd-TCG/src/header.js
+++ Dnd-TCG/src/header.js
@@ -5,12 +5,14 @@ const header = (name, className, specialization, race, background, level) =>
     .ap(
       _.section()
         .ap(
-          _.h1("pr-1 w-28 text-xs h-6 flex items-center").ap(_.span("", name)),
+          _.h1("pr-1 w-28 text-xs h-6 flex text-gray-300 items-center").ap(
+            _.span("", name),
+          ),
         )
         .ap(
           _.div("relative").ap(
             _.div(
-              "z-10 bg-gray-300 text-[0.35rem] absolute -bottom-1.5 left-1.5 px-2 rounded-b-sm",
+              "z-10 bg-gray-300 text-[0.35rem] absolute -bottom-2 left-2 px-2 leading-snug rounded-b-sm",
             )
               .ap(_.span("", className))
               .ap(_.span("", " - "))
@@ -19,12 +21,11 @@ const header = (name, className, specialization, race, background, level) =>
         ),
     )
     .ap(
-      _.div("flex flex-col px-1")
+      _.div("flex flex-col px-1 text-gray-300 mr-6")
         .ap(_.div("text-[0.35rem]", race))
         .ap(_.hr())
         .ap(_.div("text-[0.35rem]", background)),
     )
-    .ap(_.div("w-10"))
     .ap(
       _.div(
         "bg-gray-600 absolute flex justify-center items-center rounded-full right-1 top-2 w-7 h-7 border-2 border-gray-200 border-solid text-center leading-4 z-10",
